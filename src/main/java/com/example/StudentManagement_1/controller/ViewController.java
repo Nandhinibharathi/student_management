f377654862e9610f54a5f5e773523b6a928e672b
Nandhinibharathi <145819521+Nandhinibharathi@users.noreply.github.com>
2025-07-13 16:10:33 +0530

    second commit(aws)

--- src/main/java/com/example/StudentManagement_1/controller/ViewController.java
+++ src/main/java/com/example/StudentManagement_1/controller/ViewController.java
@@ -2,6 +2,7 @@ package com.example.StudentManagement_1.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,18 +10,17 @@ import org.springframework.web.bind.annotation.*;
 import com.example.StudentManagement_1.SRepository.SRepository;
 import com.example.StudentManagement_1.pathmodel.Student;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class ViewController {
-
-    private final SRepository repo;
-
-    public ViewController(SRepository repo) {
-        this.repo = repo;
-    }
+	
+	@Autowired
+     SRepository repo;
 
     @GetMapping("/login")
     public String showLoginPage() {
-        return "login";
+        return "/WEB-INF/views/login";
     }
 
     @PostMapping("/login")
@@ -28,48 +28,47 @@ public class ViewController {
                                @RequestParam String password,
                                Model model) {
         model.addAttribute("user", name);
-        return "redirect:/create";
+        return "redirect:/WEB-INF/views/create";
     }
 
     @GetMapping("/create")
     public String showCreatePage(Model model) {
         model.addAttribute("student", new Student());
         model.addAttribute("user", model.getAttribute("user"));
-        return "create";
+        return "redirect:/WEB-INF/views/create";
     }
     
     @PostMapping("/create")
     public String createStudent(@ModelAttribute Student student, Model model) {
         if (repo.existsById(student.getId())) {
             model.addAttribute("error", "ID already exists!");
-            return "create";
+            return "/WEB-INF/views/create";
         }
-        repo.save(student);
-        return "redirect:/list";
+        repo.saveAndFlush(student);
+        return "/WEB-INF/views/list";
     }
 
-
     @GetMapping("/list")
-    public String listStudents(Model model) {
+    public String listStudents(HttpServletRequest model) {
         List<Student> all = repo.findAll();
-        model.addAttribute("students", all);
-        return "list";
+        model.setAttribute("students", all);
+        return "/WEB-INF/views/list";
     }
 
     @GetMapping("/edit")
     public String editStudent(@RequestParam Integer id, Model model) {
         repo.findById(id).ifPresent(s -> model.addAttribute("student", s));
-        return "create"; // reuse form for editing
+        return "/WEB-INF/views/create"; // reuse form for editing
     }
 
-    @PostMapping("/delete")
+    @GetMapping("/delete")
     public String deleteStudent(@RequestParam Integer id) {
         repo.deleteById(id);
-        return "redirect:/list";
+        return "redirect:/WEB-INF/views/list";
     }
 
-    @GetMapping("/logout")
+    @GetMapping("/WEB-INF/views/logout")
     public String logout() {
-        return "redirect:/login";
+        return "redirect:/WEB-INF/views/login";
     }
 }
